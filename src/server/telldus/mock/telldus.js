bc3d63b08beced4de310eb1156cd96c8019f4c13
Kent Andersen <kentareandersen@gmail.com>
2014-01-23 17:03:32 +0100

    updated mock interface upgrading telldus to 0.0.7

--- src/server/telldus/mock/telldus.js
+++ src/server/telldus/mock/telldus.js
@@ -3,21 +3,45 @@ var fixtures = require("./fixtures.js");
 
 (function (exports, global) {
 
-    exports.getDevices = function() {
+    exports.getDevices = function(callback) {
+        setTimeout(function(){
+            callback(null,fixtures);
+        }, 500);
+    };
+    exports.getDevicesSync = function() {
         return fixtures;
     };
 
-    exports.turnOn = function(id) {
+    exports.turnOn = function(id, callback) {
+        setTimeout(function(){
+            callback(null);
+        }, 500);
+    };
+
+    exports.turnOnSync = function(id) {
         return 0;
     };
 
-    exports.turnOff = function(id) {
+    exports.turnOff = function(id, callback) {
+        setTimeout(function(){
+            callback(null);
+        }, 500);
+    };
+
+    exports.turnOffSync = function(id) {
         return 0;
     };
 
-    exports.dim = function(id, levl) {
+    exports.dim = function(id, level, callback) {
+        setTimeout(function(){
+            callback(null);
+        }, 500);
+    };
+
+    exports.dimSync = function(id, level) {
         return 0;
     };
+
 
     exports.addDeviceEventListener = function(callback) {
         // return tellduscore.addDeviceEventListener(callback);
