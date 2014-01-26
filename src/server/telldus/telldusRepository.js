ddaa7a4a87f354300d4da040d3ed312491c977c4
Kent Andersen <kentareandersen@gmail.com>
2014-01-26 17:08:54 +0100

    changed from hardcoded mock to env variable

--- src/server/telldus/telldusRepository.js
+++ src/server/telldus/telldusRepository.js
@@ -1,7 +1,7 @@
 var Q = require("q");
 var _ = require("underscore");
 
-var useMock = true;
+var useMock = process.env.NODE_EVN === "production";
 var telldus = require(!useMock ? "telldus" : "./mock/telldus.js");
 
 // populate all devices
