4b7c130920ccdcc517c774417f8204ff0acffed9
Kent Andersen <kentareandersen@gmail.com>
2014-01-26 19:11:11 +0100

    removed !

--- src/server/telldus/telldusRepository.js
+++ src/server/telldus/telldusRepository.js
@@ -2,7 +2,7 @@ var Q = require("q");
 var _ = require("underscore");
 
 var useMock = process.env.NODE_ENV === "production";
-var telldus = require(!useMock ? "telldus" : "./mock/telldus.js");
+var telldus = require(useMock ? "telldus" : "./mock/telldus.js");
 
 // populate all devices
 var devices = telldus.getDevicesSync();
