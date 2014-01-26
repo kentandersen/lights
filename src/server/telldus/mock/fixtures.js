6f72066292821c24ce7278d47affd257c93e19a4
Kent Andersen <kentareandersen@gmail.com>
2014-01-26 19:31:34 +0100

    updated fixtures

--- src/server/telldus/mock/fixtures.js
+++ src/server/telldus/mock/fixtures.js
@@ -2,32 +2,37 @@ var fixtures = [
     {
         name: 'Taklampe gang',
         id: 1,
-        methods: [ 'TURNON', 'TURNOFF' ],
+        methods: [ 'TURNON', 'TURNOFF', 'LEARN' ],
         model: 'selflearning-switch',
+        protocol: 'arctech',
         type: 'DEVICE',
-        status: { status: 'OFF' }
+        status: { name: 'OFF' }
     }, {
         name: 'Soverom',
         id: 2,
-        methods: [ 'TURNON', 'TURNOFF' ],
+        methods: [ 'TURNON', 'TURNOFF', 'LEARN' ],
         model: 'selflearning-switch',
+        protocol: 'arctech',
         type: 'DEVICE',
-        status: { status: 'OFF' }
+        status: { name: 'OFF' }
     }, {
         name: 'Kjokkenvask',
         id: 3,
-        methods: [ 'TURNON', 'TURNOFF' ],
+        methods: [ 'TURNON', 'TURNOFF', 'LEARN' ],
         model: 'selflearning-switch',
+        protocol: 'arctech',
         type: 'DEVICE',
-        status: { status: 'OFF' }
+        status: { name: 'ON' }
     }, {
         name: 'Stue',
         id: 4,
-        methods: [ 'TURNON', 'TURNOFF', 'DIM' ],
+        methods: [ 'TURNON', 'TURNOFF', 'DIM', 'LEARN' ],
         model: 'selflearning-dimmer',
+        protocol: 'arctech',
         type: 'DEVICE',
-        status: { status: 'DIM', level: '100' }
+        status: { name: 'ON' }
     }
 ];
 
 module.exports = fixtures;
+
