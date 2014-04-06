19425e088dde1d92258257f7e0cfbfdb998c3b1d
Kent Andersen <kentareandersen@gmail.com>
2014-04-06 13:29:37 +0200

    removed sliding view, replaced it with a button

--- src/webapp/js/modules/light/lightItemView.js
+++ src/webapp/js/modules/light/lightItemView.js
@@ -9,16 +9,8 @@ define(function(require) {
 
     var LightsView = View.extend({
 
-        options: {
-            dragThreshold: 40
-        },
-
         events: {
-            "mousedown a":      "mouseDownHandler",
-            "touchstart a":     "mouseDownHandler",
-            "webkitTransitionEnd li.animate": "animateEndHandler"
-
-            // "change .dimLevel": "dimLevelChangeHandler",
+            "click .toggle":    "toggle"
         },
 
         template: template,
@@ -28,9 +20,7 @@ define(function(require) {
         tagName: "li",
 
         initialize: function(options) {
-            _.bindAll(this, "mouseMoveHandler", "mouseDownHandler", "mouseUpHandler");
             this.listenTo(this.model, "change:status", this.statusChangeHandler);
-            //this.debounceDim = _.debounce(this.dim, 200);
         },
 
         render: function() {
@@ -40,47 +30,11 @@ define(function(require) {
             return this;
         },
 
-        mouseDownHandler: function(event) {
-            event.preventDefault();
-
-            $("body").on("mousemove touchmove", {
-                dragStartX: event.originalEvent.targetTouches ? event.originalEvent.touches[0].pageX : event.clientX,
-            }, this.mouseMoveHandler);
-
-            $("body").on("mouseup touchend", this.mouseUpHandler);
-
+        toggle: function() {
+            var status = this.model.get("status") === "ON" ? "OFF" : "ON";
+            this.model.save("status", status, {patch: true});
         },
 
-        mouseUpHandler: function(event) {
-            this.$el.addClass("animate");
-
-            $("body").off("mousemove touchmove", this.mouseMoveHandler);
-            $("body").off("mouseup touchend", this.mouseUpHandler);
-            this.$("a").css("transform", "");
-        },
-
-        mouseMoveHandler: function(event) {
-            event.preventDefault();
-            var dragDiff;
-            var $element = this.$("a");
-            if(event.originalEvent && event.originalEvent.touches) {
-                dragDiff = event.originalEvent.touches[0].pageX - event.data.dragStartX;
-            } else {
-                dragDiff = event.clientX - event.data.dragStartX;
-            }
-
-            $element.css("transform", "translate(" + dragDiff + "px,0px)");
-
-            if(dragDiff > this.options.dragThreshold ) {
-                this.model.set("status", "OFF");
-            } else if(dragDiff < -this.options.dragThreshold) {
-                this.model.set("status", "ON");
-            }
-        },
-
-        animateEndHandler: function(event) {
-            $(event.currentTarget).removeClass("animate");
-        },
 
         statusChangeHandler: function() {
             var isStatusON = this.model.get("status") === "ON";
